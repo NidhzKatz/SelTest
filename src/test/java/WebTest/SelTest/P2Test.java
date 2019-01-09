3234d054c75f13edf29dfc11b52d3e0914cf702a
NidhzKatz <46517110+NidhzKatz@users.noreply.github.com>
2019-01-09 13:16:09 +0530

    Initial commit

---
+++ src/test/java/WebTest/SelTest/P2Test.java
@@ -0,0 +1,15 @@
+package WebTest.SelTest;
+
+import org.testng.annotations.Test;
+
+public class P2Test {
+
+	@Test
+	public void Test2()
+	{
+		System.out.println("This is Test2");
+	}
+
+	
+
+}
---
+++ src/test/java/WebTest/SelTest/PractiseTest.java
@@ -0,0 +1,20 @@
+package WebTest.SelTest;
+
+import org.testng.annotations.Test;
+
+public class PractiseTest {
+	
+	
+	@Test
+	public void Test1() {
+		
+		System.out.println("This is Test1");
+		
+	}
+	
+	@Test
+	public void Test3()
+	{
+		System.out.println("This is Test3");
+	}
+}
